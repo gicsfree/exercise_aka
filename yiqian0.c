84315cc65b7c5636a83a3dcde81fa7c580caabe0
Guo lilong <guolilong2012@sohu.com>
2011-04-28 18:05:05 +0800

    fun

--- yiqian0.c
+++ yiqian0.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 int main(void)
-{
-    int i,sum=0;
-    for(i=1;i<=1000;i++)
-    {if(0==i%5)
-         {sum++;
-         if(0==i%25)
-             {sum++;
-             if(0==i%125)
-             sum++;
-             }
-         }
+{ int i,sum=0;
+    for(i=5;i<=1000;i+=5)
+     {sum++;
+     if(0==i%25)
+       {sum++;
+        if(0==i%125)
+        sum++;
+       }
      }
     printf("0 is %d\n",sum);
     return 0;
