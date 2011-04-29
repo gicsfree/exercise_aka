536ac1dc66faa825e0b006e4862cde1a8d8580af
Guo lilong <guolilong2012@sohu.com>
2011-04-29 14:39:11 +0800

    fun

--- yiqian0.c
+++ yiqian0.c
@@ -3,16 +3,18 @@ int ling(int n);
 
 int main(void)
 {
-	int i,n,sum;
-    printf("input n:\n");
-    scanf("%d",&n);
-    sum=ling(n);
+	int i, n, sum;
+	printf("input n:\n");
+	scanf("%d", &n);
+	sum = ling(n);
 	printf("0 is %d\n", sum);
 	return 0;
 }
+
 int ling(int n)
-{int i,sum=0;
-for(i=5;n/i>=1;i*=5)
-sum+=n/i;
-return sum;
+{
+	int i, sum = 0;
+	for (i = 5; n / i >= 1; i *= 5)
+		sum += n / i;
+	return sum;
 }
