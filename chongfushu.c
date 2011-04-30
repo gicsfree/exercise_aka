0df44e6bf0bfcafdaea77aedc8ab28820e9764aa
Guo lilong <guolilong2012@sohu.com>
2011-04-30 11:24:21 +0800

    fun

--- chongfushu.c
+++ chongfushu.c
@@ -2,21 +2,18 @@
 
 int main(int argc, const char *argv[])
 {
-	int i, j, k, n, m = 1, temp, b[5];
+	int i, j, k = 0, n, b[5];
 	printf("input int n:\n");
 	scanf("%d", &n);
 	if (n < 0)
 		n = 0 - n;
-	temp = n;
-	for (; temp / 10 > 0; m++)
-		temp /= 10;
-	for (i = 0; i < m; i++) {
-		b[i] = n % 10;
+	while (n) {
+		b[k++] = n % 10;
 		n /= 10;
 	}
-	for (j = 0; j < m - 1; j++)
-		for (k = j + 1; k < m; k++)
-			if (b[k] == b[j]) {
+	for (i = 0; i < k - 1; i++)
+		for (j = i + 1; j < k; j++)
+			if (b[i] == b[j]) {
 				printf("yes\n");
 				return 0;
 			}
