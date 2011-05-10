d01e2fe1d631065f8f476284ae07c02458fc8509
Guo lilong <guolilong2012@sohu.com>
2011-05-10 16:44:06 +0800

    fun

--- big_exercises/josephus/josephus.c
+++ big_exercises/josephus/josephus.c
@@ -3,20 +3,20 @@
 
 void josephus(int m, int n)
 {
-	int i, j = 0, k = 0, q = 0, a[101];
+	int i, cnt = 0, out = 0, q = 0, a[101];
 	for (i = 0; i < 101; i++)
 		a[i] = 1;
-	while (k < n) {
+	while (out < n) {
 		for (i = 1; i <= n; i++)
 			if (1 == a[i]) {
-				j++;
-				if (j == m) {
-					j = 0;
+				cnt++;
+				if (cnt == m) {
+					cnt = 0;
 					a[i] = 0;
-					k++;
+					out++;
 					{
 						q++;
-						printf("%3d:%3d out.", k,
+						printf("%3d:%3d out.", out,
 						       i);
 						if (q % 5 == 0)
 							printf("\n");
