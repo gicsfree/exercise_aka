40b51d593259b29f0967eea446b48994ae0f547d
Guo lilong <guolilong2012@sohu.com>
2011-05-15 17:25:21 +0800

    fun

---
+++ big_exercise/_josephus/josephus.h
@@ -0,0 +1,6 @@
+#ifndef __JOSEPHUS_H__
+#define __JOSEPHUS_H__
+
+int josephus(int m,int n);
+
+#endif
---
+++ big_exercise/_josephus/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN_h__
+#define __MAIN_H__
+
+enum RETURN {WRONG_IN,MEMORY_OUT,OK};
+
+#endif
