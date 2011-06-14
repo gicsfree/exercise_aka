b52c06e900d68ae62dd6bfd0bf29ed1ecc0f7770
Guo lilong <guolilong2012@sohu.com>
2011-06-14 09:31:04 +0800

    fun

--- key_point/0613/unsigned.c
+++ key_point/0613/unsigned.c
@@ -6,13 +6,18 @@ void foo(void)
 	int b = -20;
 	unsigned c = 0;
 
-	(a + b > 6) ? puts("> 6") : puts("<= 6");
+	printf("a + b = %d\n", a + b);
+	printf("a + b = %u\n", a + b);
+	(a + b > 6) ? puts("a + b > 6") : puts("a + b <= 6");	// signed converts to unsigned
 
 	c = a + b;
 	printf("c = %d\n", c);
+	printf("c = %u\n", c);
 }
 
-void main(void)
+int main(void)
 {
 	foo();
+
+	return 0;
 }
