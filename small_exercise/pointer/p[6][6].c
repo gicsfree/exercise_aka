7662fe9e40114a94db6e6053c3da3d8bbebc00ff
Guo lilong <guolilong2012@sohu.com>
2011-07-27 17:07:28 +0800

    fun

--- small_exercise/pointer/p[6][6].c
+++ small_exercise/pointer/p[6][6].c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+#define STR
+
 /* printf a string */
+#ifdef STR
 int printf_str(char (*str)[6], int n)
 {
     int iloop;
@@ -12,8 +15,24 @@ int printf_str(char (*str)[6], int n)
 
     return 0;
 }
+#else
+int printf_str(char *str[], int n)
+{
+    int iloop;
+
+    for (iloop = 0; iloop < n; iloop++)
+    {
+        printf("str[%d]\t%s\n", iloop, str[iloop]);
+    }
+
+    return 0;
+}
+#endif
+
+
 
 /* main */
+#ifdef STR
 int main(void)
 {
     char str[6][6] = {"0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"};
@@ -22,4 +41,15 @@ int main(void)
 
     return 0;
 }
+#else
+int main(void)
+{
+    char *str[6] = {"0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"};
+
+    printf_str(str, 6);
+
+    return 0;
+}
+#endif
+
 
